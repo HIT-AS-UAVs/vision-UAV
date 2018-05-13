fd9719c93a43b95a197457fd38a10bd638948ff1
hnlyzxy <hnlyzxy@qq.com>
2018-05-13 11:09:38 +0800

    可执行文件

---
+++ bui/CMakeFiles/c_uart_interface_example.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/cuart/autopilot_interface.cpp" "/home/nvidia/cuart/bui/CMakeFiles/c_uart_interface_example.dir/autopilot_interface.cpp.o"
+  "/home/nvidia/cuart/ellipse/EllipseDetectorYaed.cpp" "/home/nvidia/cuart/bui/CMakeFiles/c_uart_interface_example.dir/ellipse/EllipseDetectorYaed.cpp.o"
+  "/home/nvidia/cuart/ellipse/common.cpp" "/home/nvidia/cuart/bui/CMakeFiles/c_uart_interface_example.dir/ellipse/common.cpp.o"
+  "/home/nvidia/cuart/mavlink_control.cpp" "/home/nvidia/cuart/bui/CMakeFiles/c_uart_interface_example.dir/mavlink_control.cpp.o"
+  "/home/nvidia/cuart/serial_port.cpp" "/home/nvidia/cuart/bui/CMakeFiles/c_uart_interface_example.dir/serial_port.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
